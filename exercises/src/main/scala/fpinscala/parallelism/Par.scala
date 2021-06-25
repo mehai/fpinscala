e2f83a0eaa06a46f46afdb05b3aaa1d35887322b
Mihai Zanfir <mihaizanfir98@gmail.com>
2021-06-25 16:36:23 +0300

    parallelism full

--- exercises/src/main/scala/fpinscala/parallelism/Par.scala
+++ exercises/src/main/scala/fpinscala/parallelism/Par.scala
@@ -23,7 +23,13 @@ object Par {
       val bf = b(es)
       UnitFuture(f(af.get, bf.get)) // This implementation of `map2` does _not_ respect timeouts, and eagerly waits for the returned futures. This means that even if you have passed in "forked" arguments, using this map2 on them will make them wait. It simply passes the `ExecutorService` on to both `Par` values, waits for the results of the Futures `af` and `bf`, applies `f` to them, and wraps them in a `UnitFuture`. In order to respect timeouts, we'd need a new `Future` implementation that records the amount of time spent evaluating `af`, then subtracts that time from the available time allocated for evaluating `bf`.
     }
-  
+
+  def map3[A, B, C, D](a: Par[A], b: Par[B], c: Par[C])(f: (A, B, C) => D): Par[D] = {
+    def partF(a: A, b: B)(c: C): D = f(a, b, c)
+    val partPar: Par[C => D] = map2(a, b)((aVal, bVal) => partF(aVal, bVal))
+    map2(partPar, c)((f2, cVal) => f2(cVal))
+  }
+
   def fork[A](a: => Par[A]): Par[A] = // This is the simplest and most natural implementation of `fork`, but there are some problems with it--for one, the outer `Callable` will block waiting for the "inner" task to complete. Since this blocking occupies a thread in our thread pool, or whatever resource backs the `ExecutorService`, this implies that we're losing out on some potential parallelism. Essentially, we're using two threads when one should suffice. This is a symptom of a more serious problem with the implementation, and we will discuss this later in the chapter.
     es => es.submit(new Callable[A] { 
       def call = a(es).get
@@ -64,6 +70,44 @@ object Par {
       if (run(es)(cond).get) t(es) // Notice we are blocking on the result of `cond`.
       else f(es)
 
+  def choiceN[A](cond: Par[Int])(l: List[Par[A]]): Par[A] =
+    es =>
+      l(run(es)(cond).get)(es)
+
+  def choice2[A](cond: Par[Boolean])(t: Par[A], f: Par[A]): Par[A] =
+    choiceN( map(cond)(if (_) 0 else 1) )(List(t, f))
+
+  def choiceMap[K,V](key: Par[K])(choices: Map[K,Par[V]]): Par[V] =
+    es => {
+      val k = run(es)(key).get
+      run(es)(choices(k))
+    }
+
+  def choiceAny[K, V](cond: Par[K])(choices: K => Par[V]): Par[V] =
+    es => {
+      val key = run(es)(cond).get
+      run(es)(choices(key))
+    }
+
+  def choiceViaChoiceAny[A](cond: Par[Boolean])(t: Par[A], f: Par[A]): Par[A] =
+    choiceAny(cond)(if (_) t else f)
+
+  def choiceNViaChoiceAny[A](cond: Par[Int])(l: List[Par[A]]): Par[A] =
+    choiceAny(cond)(l(_))
+
+  def flatMap[A, B](a: Par[A])(f: A => Par[B]): Par[B] =
+    es => {
+      val key = run(es)(a).get
+      run(es)(f(key))
+    }
+
+  def join[A](a: Par[Par[A]]): Par[A] = es => run(es)(run(es)(a).get())
+
+  def joinViaFlatMap[A](a: Par[Par[A]]): Par[A] = flatMap(a)(x => x)
+
+  def flatMapViaJoin[A, B](a: Par[A])(f: A => Par[B]): Par[B] =
+    join(map(a)(f))
+
   /* Gives us infix syntax for `Par`. */
   implicit def toParOps[A](p: Par[A]): ParOps[A] = new ParOps(p)
 
@@ -83,11 +127,24 @@ object Examples {
       sum(l) + sum(r) // Recursively sum both halves and add the results together.
     }
 
+  def parProc[A](arr: IndexedSeq[A], baseCase: A, f: (A, A) => A): Par[A] =
+    if (arr.size <= 1)
+      Par.unit[A](arr.headOption getOrElse baseCase)
+    else {
+      val (l, r) = arr.splitAt(arr.length / 2)
+      Par.map2(parProc(l, baseCase, f), parProc(r, baseCase, f))(f(_, _))
+    }
+
   def max(ints: IndexedSeq[Int]): Par[Int] =
     if (ints.size <= 1)
       Par.unit[Int](ints.headOption getOrElse Int.MinValue)
     else {
       val (l,r) = ints.splitAt(ints.length/2)
-      Par.unit(Par.map2(Par.fork(max(l)), Par.fork(max(r)))(_ max _))
+      Par.map2(max(l), max(r))(_ max _)
     }
+
+  def countWords(paragraphs: List[String]): Par[Int] = {
+    val wordCountList: Par[List[Int]] = Par.parMap(paragraphs)(_.split(" ").length)
+    map(wordCountList)(lst => lst.sum)
+  }
 }
